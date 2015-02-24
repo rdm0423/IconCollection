1ce84b70ea3f39c411bdc35862aae912626eeebf
Taylor Mott <trm36@humboldt.edu>
2015-02-24 01:40:42 -0700

    Initial Commit With Pictures
    Signed-off-by: Taylor Mott <trm36@humboldt.edu>

---
+++ IconCollection/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  IconCollection
+//
+//  Created by Taylor Mott on 24.2.2015.
+//  Copyright (c) 2015 DevMountain. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ IconCollection/ViewController.m
@@ -0,0 +1,27 @@
+//
+//  ViewController.m
+//  IconCollection
+//
+//  Created by Taylor Mott on 24.2.2015.
+//  Copyright (c) 2015 DevMountain. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
